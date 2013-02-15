d7624b94dee23ce21cba0a6eead87163aae389cb
Heikki Verta <heikki.verta@gmail.com>
2013-02-15 21:40:25 +0200

    Pass message to irc on create Update unread message count

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,9 @@ class MessagesController < ApplicationController
     @channel = (params[:channel]) ? params[:channel] : '#ep-dev'
     @messages = Message.list(@channel)
     @private_message_senders = Message.private_message_senders
+    unreadMessages = $redis.get('channel:' + @channel + ':unreadmessagecount')
+    $redis.set('channel:' + @channel + ':unreadmessagecount', '0')
+    puts "#{unreadMessages} unread messages in channel #{@channel}"
     respond_to do |format|
       format.html
       format.json { render :json => @messages }
@@ -15,8 +18,13 @@ class MessagesController < ApplicationController
   end
 
   def create
+    channel = params['channel']
     @m = Message.new(params)
+    $bot.Channel(channel).msg(@m.body)
     Message.save(@m)
+    $redis.incr('channel:' + channel + ':messagecount')
+    $redis.incr('channel:' + channel + ':unreadmessagecount')
+    puts "Sent message to #{channel}"
     redirect_to :action => "index", :channel => params[:channel]
   end
 
