4d889b0896bdfcd1193435549fad76249653f67d
Heikki Verta <heikki@applifier.com>
2013-02-18 13:39:58 +0200

    Print redis URL

--- config/initializers/01_redis.rb
+++ config/initializers/01_redis.rb
@@ -1,3 +1,4 @@
+puts "Redis URL: #{ENV['REDISTOGO_URL']}"
 uri = URI.parse(ENV["REDISTOGO_URL"])
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 puts 'Redis connection opened'
