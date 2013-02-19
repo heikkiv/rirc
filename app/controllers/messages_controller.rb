15701f7a63bc178cfad0d0af6220bac7ec753c23
Heikki Verta <heikki@applifier.com>
2013-02-19 12:15:14 +0200

    Don't try to load users for private messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
     @channel = (params[:channel]) ? params[:channel] : '#ep-dev'
     @since = (params[:since]) ? params[:since].to_f : 0;
     @messages = Message.list(@channel, @since)
-    @users = $bot.Channel(@channel).users.keys
+    @users = (@channel.start_with?('#')) ? $bot.Channel(@channel).users.keys : []
 
     unread_messages = $redis.get('channel:' + @channel + ':unreadmessagecount')
     $redis.set('channel:' + @channel + ':unreadmessagecount', '0')
