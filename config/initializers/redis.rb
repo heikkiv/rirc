5076b9bc3df61b6e9090638b3f558659f84e3bd6
Heikki Verta <heikki@applifier.com>
2013-02-19 10:01:42 +0200

    Use localhost if REDISTOGO_URL is not defined

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-puts "Redis URL: #{ENV['REDISTOGO_URL']}"
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = (ENV['REDISTOGO_URL']) ? URI.parse(ENV['REDISTOGO_URL']) : URI.parse('redis://localhost:6379/')
+puts "Redis URL: #{uri}"
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 puts 'Redis connection opened'
